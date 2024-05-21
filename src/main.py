8ebefeedfa40f4008ca388b0d6ddc1d66cd6af76
GearMic <mic.lp281@gmail.com>
2024-05-21 15:23:43 +0200

    proper rolling and converting float to complex

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import h5py
 import pathlib
+import re
 
 def load_mean_data(rawFilename, arrFilename, forceGenerate=False):
     # load saved np array and return if it exists
@@ -15,15 +16,29 @@ def load_mean_data(rawFilename, arrFilename, forceGenerate=False):
     rawFile = h5py.File(rawFilename, 'r')
     stream = rawFile['stream_a']
     confs_list = []
+
+    tRegex = re.compile('t\\d+_') # regular expression to find t value
     for conf in stream.values():
         lattices = []
         for item in conf.items():
-            # print(item[0])
-            lattice = item[1]
-            lattices.append(np.array(lattice).squeeze())
+            tMatch = tRegex.match(item[0])
+            # extract t value
+            if tMatch is None:
+                print("ERROR: couldn't find t value in %s" % item[0])
+            t = int(tMatch.group()[1:-1])
+
+            #convert to complex array
+            latticeFloat = np.array(item[1]).squeeze()
+            latticeComplex = np.empty(len(latticeFloat)//2, np.cdouble)
+            latticeComplex.real = latticeFloat[0::2]
+            latticeComplex.imag = latticeFloat[1::2]
+            # print(latticeComplex)
+            # print(item[0], t, np.array(lattice).shape)
+            # add rolled array to the list
+            lattices.append(np.roll(latticeComplex, -t))
 
         # confs_list.append(np.mean(np.array(lattices), 0))
-        confs_list.append(np.array(lattices)[0])
+        confs_list.append(np.array(lattices)[5])
     confs = np.array(confs_list)
 
     # save np array
@@ -37,8 +52,8 @@ rawFilename = '../data/pion.local-local.u-gf-d-gi.px0_py0_pz0.h5'
 arrFilename = '../data/confs.npy'
 confs = load_mean_data(rawFilename, arrFilename, True)
 
-# conf = confs[2][0::2]
-conf = np.roll(confs[0][0::2], 10)
+# conf = confs[0][0::2]
+conf = confs[0]
 tau = np.arange(len(conf))
 plt.plot(tau, conf)
 plt.plot(tau, conf, 'x')
